1a20b5b41fd4da5e18d19a54758bf1a993916d47
CircleWang <57208136+Circle-wang@users.noreply.github.com>
2023-02-14 14:04:40 +0800

    2.14

--- Multi_Thread/Chapter2/ThreadState.java
+++ Multi_Thread/Chapter2/ThreadState.java
@@ -1,7 +1,7 @@
-package Demo7;
+package Chapter2;
 
 // 观察测试线程的状态
-public class TestState {
+public class ThreadState {
 
     public static void main(String[] args) throws InterruptedException {
         Thread myThread = new Thread(()->{
@@ -13,23 +13,24 @@ public class TestState {
                 }
             }
             System.out.println(Thread.currentThread().getName()+ "已经结束");
-        });
+        });  // 采用lambda表达式创建一个线程
 
         // 返回该线程的状态myThread.getState()
         Thread.State myState = myThread.getState();
-        System.out.println(myState); // 此时状态应该是NEW
+        System.out.println("线程启动前状态:" + myState); // 此时状态应该是NEW
 
         // 观察启动后的状态
         myThread.start();
         myState = myThread.getState();
-        System.out.println(myState); // 此时状态应该是RUN
+        System.out.println("线程start()后状态:" + myState); // 此时状态应该是RUN
 
-        // Thread.State.TERMINATED 表示线程终止
+        // 每500ms打印一次线程状态
         while (myState != Thread.State.TERMINATED) {
-            Thread.sleep(100);     // 让主线程慢一点
+            Thread.sleep(500);      
             myState = myThread.getState(); // 更新线程状态
             System.out.println(myState);   // 此时状态应该是TIMED_WAITING
         }
+        
     }
 
 
